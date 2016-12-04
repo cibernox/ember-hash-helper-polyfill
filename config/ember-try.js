1cad50c4a389cf81522aecd4911b945f161027c8
cibernox <miguel.camba@gmail.com>
2016-12-04 12:05:36 +0000

    Update to ember-cli 2.11
    To make sure this is compatible with ember-source from NPM
    
    Also removes the test.
    
    I don't know how to conditionally run tests in certain versions of ember,
    and given the little value that this silly test was adding to the addon,
    it's better to remove it and move on.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,44 +1,62 @@
+/*jshint node:true*/
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      dependencies: { }
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '1.13.10',
+          'ember-cli-shims': '0.1.3'
+        },
+        resolutions: {
+          'ember': '1.13.10'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4',
+          'ember-cli-shims': '0.1.3'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
     },
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release',
+          'ember-cli-shims': '0.1.3'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
       },
-      resolutions: {
-        'ember': 'release'
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
       }
     },
     {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
       }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    },
-    {
-      name: 'ember-1.13.10',
-      dependencies: {
-        'ember': '1.13.10'
-      },
-      resolutions: {
-        'ember': '1.13.10'
-      },
     }
   ]
 };
